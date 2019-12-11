8e3273aea2cfd505cffd1efb5ce5c9e685eb7b96
Maxwell Acioli <maxwell.acioli@gmail.com>
2019-12-11 09:24:25 -0300

    Added request to return authenticated current user

--- src/main/java/com/maxwell/AbasteceLegal/controller/AuthRestAPIs.java
+++ src/main/java/com/maxwell/AbasteceLegal/controller/AuthRestAPIs.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -87,5 +88,10 @@ public class AuthRestAPIs {
         userRepository.save(user);
 
         return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> index(@AuthenticationPrincipal UserPrinciple userPrinciple) {
+        return ResponseEntity.ok(userPrinciple);
     }
 }
